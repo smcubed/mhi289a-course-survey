22372727ccd7a23577180dfa9f9d8248107a0dc9
Scott McGrath <smcgrath1@gmail.com>
2026-09-12 14:12:12 -0600

    Update APPS_SCRIPT_URL in config.js
    Added script url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // The only file you should need to edit after deploying the Apps Script.
 export const CONFIG = {
   // Paste the Web app URL from Apps Script (ends in /exec). See apps-script/SETUP.md.
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzGgr6i-DD3edAsFp27u0Bvtzhm1G_Hw9ZQ-G1gCy_bIk060qff26Ikn1UFfjb7jrI7og/exec",
 
   // Bump this whenever you change questions so responses from different versions
   // can be told apart in the Sheet.
